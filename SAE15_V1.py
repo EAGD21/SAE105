9c234395cafa4b97d86a2ee004117199b2faa244
EAGD21 <eagd61@gmail.com>
2026-01-15 14:37:38 +0100

    début de la map v1

--- SAE15_V1.py
+++ SAE15_V1.py
@@ -56,9 +56,9 @@ def node_to_md(id):
 
     #headers = {"User-Agent": "CityDataScript/1.0 (contact@example.org)"}
     #wp_res = requests.get(f"https://tile.openstreetmap.org/{data[2]}/{data[3]}/15.png")
-    
-    data = print_node_attributes(id)
+
     n = len(data)
+    data = get_node(id)
     txt = ""
     txt += f"# Type: {data[0]}  \n# ID: {data[1]}  \n# Latitude: {data[2]}  \n# Longitude: {data[3]}  \n\n\n"
     txt += f"![map](https://tile.openstreetmap.org/{data[2]}/{data[3]}/15.png) \n\n"
@@ -66,8 +66,17 @@ def node_to_md(id):
         txt += f" - **{data[i][0]}:** {data[i][1]}  \n\n"
     with open('file.md','w') as f:
         f.write(txt)
+
+def generate_map(lat,lon):
+    x = (lon + 180) / 360 * 256 * 2**15
+    y = (1 - ( (lat * 3.141592653589793 / 180).tan() + (1 / (lat * 3.141592653589793 / 180).cos()).ln() ) / 3.141592653589793) / 2 * 256 * 2**15
+    tile_x = int(x // 256)
+    tile_y = int(y // 256)
+    return f"https://tile.openstreetmap.org/15/{tile_x}/{tile_y}.png"
+
+def fiche_osm(id):
+    node_to_md(id)
     convert()
-    return c'est bon
 
 def evaluation(ville):
     ville = ville.title()
@@ -248,4 +257,3 @@ https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];
     out geom;.json
 
 '''
-
